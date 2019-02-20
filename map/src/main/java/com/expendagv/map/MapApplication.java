e1bdba7674a2dfd59c023169625ba813e0a0baf6
maxiaonan <maxiaonan@cmhi.chinamobile.com>
2019-02-20 10:01:24 +0800

    add new module
    add map /core/path /task/util module to project

---
+++ map/src/main/java/com/expendagv/map/MapApplication.java
@@ -0,0 +1,13 @@
+package com.expendagv.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+    }
+
+}
